9feceeb6647c912c31479fe25429eed3a8e1dab9
Rash449 <rashmikadilmin499@gmail.com>
2025-08-11 00:16:58 +0530

    code modified

--- src/auth.rs
+++ src/auth.rs
@@ -1,25 +1,25 @@
-use serde::{Deserialize, Serialize};
-use chrono::preliude::*;
-use jsonwebtoken::{encode, decode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use crate::{error::Error, Result, WebResult};
+use chrono::prelude::*;
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
 use std::fmt;
 use warp::{
-    fliters::header::headers_cloned,
+    filters::header::headers_cloned,
     http::header::{HeaderMap, HeaderValue, AUTHORIZATION},
     reject, Filter, Rejection,
-}
+};
 
-const BEARER: &str = "Bearer";
-const JWT_SECRET: &[u8] = b"secret_key";
+const BEARER: &str = "Bearer ";
+const JWT_SECRET: &[u8] = b"secret";
 
 #[derive(Clone, PartialEq)]
 pub enum Role {
-    Admin,
     User,
+    Admin,
 }
 
-impl Role{
-    pub fn from_str(role: &str) -> Role{
+impl Role {
+    pub fn from_str(role: &str) -> Role {
         match role {
             "Admin" => Role::Admin,
             _ => Role::User,
@@ -30,48 +30,42 @@ impl Role{
 impl fmt::Display for Role {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Role::Admin => write!(f, "Admin"),
             Role::User => write!(f, "User"),
+            Role::Admin => write!(f, "Admin"),
         }
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims{
+#[derive(Debug, Deserialize, Serialize)]
+struct Claims {
     sub: String,
     role: String,
     exp: usize,
 }
 
 pub fn with_auth(role: Role) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
-    warp::header::headers_cloned()
-        .and_then(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
+    headers_cloned()
+        .map(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
         .and_then(authorize)
 }
 
-pub fn create_token(uid: &str, role: &Role) -> Result<String> {
-
-    let expression = Utc::now()
+pub fn create_jwt(uid: &str, role: &Role) -> Result<String> {
+    let expiration = Utc::now()
         .checked_add_signed(chrono::Duration::seconds(60))
         .expect("valid timestamp")
         .timestamp();
 
     let claims = Claims {
-        sub: uid.to_string(),
+        sub: uid.to_owned(),
         role: role.to_string(),
-        exp: expression as usize,
+        exp: expiration as usize,
     };
-
     let header = Header::new(Algorithm::HS512);
     encode(&header, &claims, &EncodingKey::from_secret(JWT_SECRET))
-        .map_err(|e| Error::JWTTokenCreationError(e.to_string()));
-    
+        .map_err(|_| Error::JWTTokenCreationError)
 }
 
-async fn authorize(
-    role: Role,
-    headers: HeaderMap<HeaderValue>,
-) -> WebResult<String> {
+async fn authorize((role, headers): (Role, HeaderMap<HeaderValue>)) -> WebResult<String> {
     match jwt_from_header(&headers) {
         Ok(jwt) => {
             let decoded = decode::<Claims>(
@@ -79,11 +73,12 @@ async fn authorize(
                 &DecodingKey::from_secret(JWT_SECRET),
                 &Validation::new(Algorithm::HS512),
             )
-            .map_err(|e| reject::custom(Error::JWTTokenError))?;
+            .map_err(|_| reject::custom(Error::JWTTokenError))?;
 
             if role == Role::Admin && Role::from_str(&decoded.claims.role) != Role::Admin {
                 return Err(reject::custom(Error::NoPermissionError));
             }
+
             Ok(decoded.claims.sub)
         }
         Err(e) => return Err(reject::custom(e)),
@@ -92,15 +87,15 @@ async fn authorize(
 
 fn jwt_from_header(headers: &HeaderMap<HeaderValue>) -> Result<String> {
     let header = match headers.get(AUTHORIZATION) {
-        Some(header) => header,
-        None => return Err(Error::MissingAuthorizationHeader),
+        Some(v) => v,
+        None => return Err(Error::NoAuthHeaderError),
     };
     let auth_header = match std::str::from_utf8(header.as_bytes()) {
-        Ok(header) => header,
-        Err(_) => return Err(Error::InvalidAuthorizationHeader),
+        Ok(v) => v,
+        Err(_) => return Err(Error::NoAuthHeaderError),
     };
     if !auth_header.starts_with(BEARER) {
-        return Err(Error::InvalidAuthorizationHeader);
+        return Err(Error::InvalidAuthHeaderError);
     }
     Ok(auth_header.trim_start_matches(BEARER).to_owned())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-use error::Error;
 use auth::{with_auth, Role};
+use error::Error::*;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::sync::Arc;
-use tokio::sync::Mutex;
-use warp::{
-    Filter,
-    reject,
-    reply,
-    Rejection,
-    Reply,
-};
+use warp::{reject, reply, Filter, Rejection, Reply};
 
 mod auth;
 mod error;
@@ -21,7 +14,7 @@ type WebResult<T> = std::result::Result<T, Rejection>;
 type Users = Arc<HashMap<String, User>>;
 
 #[derive(Clone)]
-pub struct User{
+pub struct User {
     pub uid: String,
     pub email: String,
     pub pw: String,
@@ -49,63 +42,55 @@ async fn main() {
         .and(warp::body::json())
         .and_then(login_handler);
 
-    let users_route = warp::path!("user")
-        .and(with_auth(Roles::User))
+    let user_route = warp::path!("user")
+        .and(with_auth(Role::User))
         .and_then(user_handler);
-
     let admin_route = warp::path!("admin")
-        .and(with_auth(Roles::Admin))
+        .and(with_auth(Role::Admin))
         .and_then(admin_handler);
 
     let routes = login_route
-        .or(users_route)
+        .or(user_route)
         .or(admin_route)
         .recover(error::handle_rejection);
 
-
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 8080))
-        .await;
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
 
 fn with_users(users: Users) -> impl Filter<Extract = (Users,), Error = Infallible> + Clone {
     warp::any().map(move || users.clone())
 }
 
-pub async fn login_handler(
-    users: Users,
-    body: Login Request,
-) -> WebResult<impl Reply>{
+pub async fn login_handler(users: Users, body: LoginRequest) -> WebResult<impl Reply> {
     match users
         .iter()
-        .find(|(_uid, user)| user.email == body.email && user.pw == body.pw) {
-            some((uid, user)) => {
-                let token = auth::create_token(&uid, &Role::from_str(&user.role));
+        .find(|(_uid, user)| user.email == body.email && user.pw == body.pw)
+    {
+        Some((uid, user)) => {
+            let token = auth::create_jwt(&uid, &Role::from_str(&user.role))
                 .map_err(|e| reject::custom(e))?;
-                Ok(reply::json(&LoginResponse { token }))
-            }
-            None => {
-                Err(reject::custom(WrongCredentialsError)),
-            }
+            Ok(reply::json(&LoginResponse { token }))
         }
+        None => Err(reject::custom(WrongCredentialsError)),
+    }
 }
 
 pub async fn user_handler(uid: String) -> WebResult<impl Reply> {
-    Ok(format!("User profile for UID: {}", uid))
+    Ok(format!("Hello User {}", uid))
 }
 
 pub async fn admin_handler(uid: String) -> WebResult<impl Reply> {
-    Ok(format!("Admin dashboard for UID: {}", uid))
+    Ok(format!("Hello Admin {}", uid))
 }
 
 fn init_users() -> HashMap<String, User> {
-    let mut users = HashMap::new();
+    let mut map = HashMap::new();
     map.insert(
         String::from("1"),
         User {
             uid: String::from("1"),
-            email: String::from("rashmikadlinmin499@gmail.com"),
-            pw: String::from("password123"),
+            email: String::from("rashmika@gmail.com"),
+            pw: String::from("admin1234"),
             role: String::from("User"),
         },
     );
@@ -113,12 +98,10 @@ fn init_users() -> HashMap<String, User> {
         String::from("2"),
         User {
             uid: String::from("2"),
-            email: String::from("Admin@admin.com"),
-            pw: String::from("admin1234"),
+            email: String::from("admin@adminaty.com"),
+            pw: String::from("4321"),
             role: String::from("Admin"),
         },
     );
+    map
 }
-
-
-
